84cb868a68f116e3676a244918097d04d9d569f5
Lad <ladeadeloye@gmail.com>
2022-04-08 17:31:26 -0700

    Positive or neg c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,16 +7,10 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
-
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *
- *
- *    * Return: Always 0 (Success)
- *
- *
+ * Return: Always 0 (Success)
  */
 
 int main(void)

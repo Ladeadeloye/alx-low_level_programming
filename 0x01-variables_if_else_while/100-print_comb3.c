d12fd12a0337da6f5a5d9f181b730b8972e64ff1
Lad <ladeadeloye@gmail.com>
2022-04-10 08:38:51 -0700

    Combination of brains

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,8 +8,8 @@ nclude <stdio.h>
  * different combinations of two digits
  * Numbers must be separated by , followed by a space
  * The two digits must be different
- * 01 and 10 are considered the same combination of the two digits 0 and 1
- * Print only the smallest combination of two digits
+ * 01 and 10 are considered the same combination of the two dig
+ * ts 0 and 1 Print only the smallest combination of two digits
  * Numbers should be printed in ascending order, with two digits
  * You can only use the putchar function
  * (every other function (printf, puts, etc…) is forbidden)
@@ -20,53 +20,30 @@ nclude <stdio.h>
  */
 
 int main(void)
-
 {
-
 	int c;
-
 	int d = 0;
-
 	while (d < 10)
 
 	{
-
 		c = 0;
-
 		while (c < 10)
 
 		{
-
 			if (d != c && d < c)
-
 			{
-
 				putchar('0' + d);
-
 				putchar('0' + c);
-
 				if (c + d != 17)
-
 				{
-
 					putchar(',');
-
 					putchar(' ');
-
 				}
-
 			}
-
 			c++;
-
 		}
-
 		d++;
-
 	}
-
 	putchar('\n');
-
 	return (0);
-
 }

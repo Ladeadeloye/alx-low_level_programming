6f9dfaf016811a68e66e9da0fb96bf9460a8c46d
Lad <ladeadeloye@gmail.com>
2022-04-10 08:28:47 -0700

    Success combination in business

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -24,8 +24,9 @@ int main(void)
 	int c;
 	int d;
 	int e = 0;
+
 	while (e < 10)
-	{	
+	{
 		d = 0;
 		while (d < 10)
 		{
@@ -37,7 +38,8 @@ int main(void)
 					putchar('0' + e);
 					putchar('0' + d);
 					putchar('0' + c);
-if (c + d + e != 9 + 8 + 7)
+
+					if (c + d + e != 9 + 8 + 7)
 					{
 						putchar(',');
 						putchar(' ');		}

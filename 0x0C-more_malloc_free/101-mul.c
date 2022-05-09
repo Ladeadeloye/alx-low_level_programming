6217ddf9d3ba2fbcd723162b8d52bff06cb81b10
Ladeadeloye <ladeadeloye@gmail.com>
2022-05-09 20:38:40 +0100

    Multiply two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -12,7 +12,7 @@ int num1, num2;
 	if (argc != 3)
 	{ printf("Error\n");
 	exit(98); }
-	for (num1 = 1; num1 < argc; i++)
+	for (num1 = 1; num1 < argc; num1++)
 	{
 		for (num2 = 0; argv[num1][num2] != '\0'; num2++)
 		{
